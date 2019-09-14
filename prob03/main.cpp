f38f2a0854a0f73b9e24a9ceaa98a4bf627eeb20
willyr2501 <willyr2501@cs104u-3a.acad.fullerton.edu>
2019-09-14 14:49:16 -0700

    finished

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,14 @@
 // Sales prediction
+#include <iostream>
+
+int main()
+{
+  const int TOTAL = 2103419277;
+  const double PERCENT = 0.18;
+
+  //Find percentage by which sales have increased in year 2019
+  long prediction = (TOTAL * PERCENT) + TOTAL;
+  std::cout << "Last year's sales were: $" << TOTAL << std::endl;
+  std::cout << "This year's sales prediction: $" << prediction << std::endl;
+  return 0;
+}
